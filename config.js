32904091a34c1ece0c8bca58916b60ff7f5d66d7
ngducanh2k <85271937+ngducanh2k@users.noreply.github.com>
2021-06-04 02:49:16 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'các bạn à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao các bạn cứ đi nhầm đường
+    Lạc vào chỗ này thế 
+    Đằng sau cmt ngu dốt`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng các bạn thích nơi đây 😙',
+    desc: 'Phải chăng các bạn chọn trang này ',
+    btnYes: 'Vẫn cứ sống ảo đi <33',
+    btnNo: 'Không, sống ảo à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà các bạn hư thế <3',
+    btnReply: 'Gửi cho các bạn <3',
+    reply: 'hư thì hư ko hư thì hư <33333333',
+    mess: 'mình biết mà 🥰. các bạn thật hư 😘😘',
+    messDesc: 'Tối nay các bạn đừng có hư nhé.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100030327638673' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100005467518085/
+}
